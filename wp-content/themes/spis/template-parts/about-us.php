b27bcd8abaea82e7c5c4764046e0fbf623693de9
Nenad Mihajlovic <nenad.mihajlovic@holistic-digital.com>
2025-12-24 17:19:48 +0100

    Work on goals section

--- wp-content/themes/spis/template-parts/about-us.php
+++ wp-content/themes/spis/template-parts/about-us.php
@@ -9,6 +9,42 @@ get_header();
     <div class="about-hero-container"></div>
 </section>
 
+<!-- Goals -->
+<section class="about-goals-main page-width-full">
+    <div class="about-goals-container page-width-boxed-5">
+        <div class="about-goals-title">
+            <p>SPIS 2026.</p>
+        </div>
+        <div class="about-goals-content">
+            <div class="about-goals-item">
+                <div class="about-goals-item-1">
+                    <span>/01</span>
+                    <p>Promovisanje legalne i odgovorne igre na sreću</p>
+                </div>
+            </div>
+            <div class="about-goals-item">
+                <div class="about-goals-item-2">
+                    <span>/02</span>
+                    <p>Zastupanje zajedničkih interesa članica pred državnim institucijama</p>
+                </div>
+            </div>
+            <div class="about-goals-item">
+                <div class="about-goals-item-3">
+                    <span>/03</span>
+                    <p>Podsticanje transparentnosti i najbolje prakse u industriji</p>
+                </div>
+            </div>
+            <div class="about-goals-item">
+                <div class="about-goals-item-4">
+                    <span>/04</span>
+                    <p>Informisanje javnosti o doprinosu industrije ekonomiji i zajednici</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+
 <!--History -->
 <section class="about-history-main page-width-full">
     <div class="about-history-container page-width-boxed">
@@ -26,28 +62,39 @@ get_header();
     <div class="about-strategy-container page-width-boxed-5">
         <div class="about-strategy-row">
             <div class="about-strategy-row-content">
-                <img src="/wp-content/uploads/2025/12/ico_razvoj-1.svg" alt="ico razvoj">
+                <div class="about-strategy-icon">
+                    <img src="/wp-content/uploads/2025/12/ico_razvoj-1.svg" alt="ico razvoj">
+                </div>
                 <h3>Razvoj industrije</h3>
-                <p>uklanjanje barijera, podsticanje inovacija i unapređenjeposlovnog okruženja</p>
+                <p>uklanjanje barijera, podsticanje inovacija i unapređenje poslovnog okruženja</p>
             </div>
+
             <div class="about-strategy-row-content">
-                <img src="/wp-content/uploads/2025/12/ico_odg_priredjivanje-1.svg" alt="odgovornost">
+                <div class="about-strategy-icon">
+                    <img src="/wp-content/uploads/2025/12/ico_odg_priredjivanje-1.svg" alt="odgovornost">
+                </div>
                 <h3>Odgovorno priređivanje</h3>
                 <p>sprovođenje i promocija visokih standarda zaštite igrača i sprečavanje maloletničkog kockanja</p>
             </div>
+
             <div class="about-strategy-row-content">
-                <img src="/wp-content/uploads/2025/12/ico_zakons-1.svg" alt="zakonitost">
+                <div class="about-strategy-icon">
+                    <img src="/wp-content/uploads/2025/12/ico_zakons-1.svg" alt="zakonitost">
+                </div>
                 <h3>Zastupanje i regulativa</h3>
-                <p>saradnja sa regulatorima i zakonodavcimaradi izgradnje fer i modernog zakonskog okvira</p>
+                <p>saradnja sa regulatorima i zakonodavcima radi izgradnje fer i modernog zakonskog okvira</p>
             </div>
+
             <div class="about-strategy-row-content">
-                <img src="/wp-content/uploads/2025/12/ico_drustv-1.svg" alt="društvena odgovornost">
+                <div class="about-strategy-icon">
+                    <img src="/wp-content/uploads/2025/12/ico_drustv-1.svg" alt="društvena odgovornost">
+                </div>
                 <h3>Društvena odgovornost</h3>
                 <p>ulaganje u zajednicu kroz humanitarne akcije, donacije i projekte koji doprinose društvu</p>
             </div>
         </div>
-        <div>
-            <h2>Strategija</h2>
+        <div class="about-strategy-row-bottom">
+            <h2 class="about-strategy-bottom-title">Strategija</h2>
         </div>
     </div>
 </section>
